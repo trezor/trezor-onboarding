760592f422a077b2288e5efb81320ad5e59c2f56
martin <mvarmuza@gmail.com>
2019-05-03 12:56:31 +0200

    fix newsletter

--- src/config/mailchimp.js
+++ src/config/mailchimp.js
@@ -1,9 +1,12 @@
 
 // eslint-disable-next-line prefer-destructuring
-export const MAILCHIMP_U = process.env.MAILCHIMP_U;
+export const MAILCHIMP_U = 'a87eb6070c965ef1be1b02854';
 // eslint-disable-next-line prefer-destructuring
-export const MAILCHIMP_ID = process.env.MAILCHIMP_ID;
+export const MAILCHIMP_ID = '0ac8b24e69';
 // eslint-disable-next-line prefer-destructuring
-export const MAILCHIMP_BASE = process.env.MAILCHIMP_BASE;
+export const MAILCHIMP_BASE = 'https://trezor.us7.list-manage.com';
 // eslint-disable-next-line prefer-destructuring
-export const MAILCHIMP_GROUP_ID = process.env.MAILCHIMP_GROUP_ID;
+export const MAILCHIMP_GROUP_ID = '1'; // todo: fix
+
+// from bach
+// const MAILCHIMP_URL = '//trezor.us7.list-manage.com/subscribe/post-json?u=a87eb6070c965ef1be1b02854&id=0ac8b24e69&c=?&group[1][2]=true&group[5][8]=true&group[21][32]=true&group[21][64]=true&group[21][128]=true&group[21][256]=true&group[21][512]=true&EMAIL=';
