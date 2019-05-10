ab96114d5237bbfb853260e8a3ec0b02d11bf44d
martin <mvarmuza@gmail.com>
2019-05-10 15:24:41 +0200

    remove some unused dependencies

--- test/health/urls.test.js
+++ test/health/urls.test.js
@@ -6,12 +6,9 @@ describe('Test that all external links are alive', () => {
     beforeEach(() => {
         jest.setTimeout(20000);
     });
-    // const resp = await fetch('https://github.com/');
     Object.values(URLS).forEach((url) => {
         it(`HTTP GET request to ${url} should respond with 200`, async () => {
-            console.warn(url);
             const response = await fetch(url);
-            console.warn(response.status);
             expect(response.status).toEqual(200);
         });
     });
